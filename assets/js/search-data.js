ac781afc0b3850be34deac1894e10ab79069337f
wortola5 <115573502+wortola5@users.noreply.github.com>
2025-07-23 01:57:51 +0000

    Deploying to gh-pages from @ wortola5/ortola@6ffa841a891a3233d13fc27abb00358256f8436e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -515,7 +515,7 @@ ninja.data = [{
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("/ortola/feed.xml", "_blank");
+          window.open("/ortola//feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
